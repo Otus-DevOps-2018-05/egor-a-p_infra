e4e9864df3fe7d27c5713fc1f30da026b19a7a24
Egor Petrov <egor.a.petrov@gmail.com>
2018-07-08 13:41:44 +0300

    Parametrize vpc

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -9,6 +9,5 @@ resource "google_compute_firewall" "firewall_ssh" {
       "22"]
   }
 
-  source_ranges = [
-    "0.0.0.0/0"]
+  source_ranges = "${var.source_ranges}"
 }
---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,6 @@
+variable source_ranges {
+  type = "list"
+  description = "Allowed IP addresses"
+  default = [
+    "0.0.0.0/0"]
+}
