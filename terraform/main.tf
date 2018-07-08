ba46d3465d5d7ddf4b28ac75342dc491923c29c1
Egor Petrov <egor.a.petrov@gmail.com>
2018-07-08 13:37:30 +0300

    Add modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,90 +4,20 @@ provider "google" {
   region = "${var.region}"
 }
 
-resource "google_compute_instance" "app" {
-  name = "reddit-app${count.index}"
-  machine_type = "g1-small"
+module "app" {
+  source = "modules/app"
+  public_key_path = "${var.public_key_path}"
   zone = "${var.zone}"
-  count = "${var.count}"
-
-  tags = [
-    "reddit-app",
-  ]
-
-  boot_disk {
-    initialize_params {
-      image = "${var.disk_image}"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      nat_ip = "${google_compute_address.app_ip.address}"
-    }
-  }
-
-  metadata {
-    ssh-keys = "appuser:${file(var.public_key_path)}appuser1:${file(var.public_key_path)}appuser2:${file(var.public_key_path)}"
-  }
-
-  connection {
-    type = "ssh"
-    user = "appuser"
-    agent = false
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "file" {
-    source = "files/puma.service"
-    destination = "/tmp/puma.service"
-  }
-
-  provisioner "remote-exec" {
-    script = "files/deploy.sh"
-  }
+  app_disk_image = "${var.app_disk_image}"
 }
 
-resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
-
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-
-    ports = [
-      "9292",
-    ]
-  }
-
-  source_ranges = [
-    "0.0.0.0/0",
-  ]
-
-  target_tags = [
-    "reddit-app",
-  ]
+module "db" {
+  source = "modules/db"
+  public_key_path = "${var.public_key_path}"
+  zone = "${var.zone}"
+  db_disk_image = "${var.db_disk_image}"
 }
 
-resource "google_compute_firewall" "firewall_ssh" {
-  name = "default-allow-ssh"
-
-  description = "Allow SSH from anywhere"
-
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "22"]
-  }
-
-  source_ranges = [
-    "0.0.0.0/0"]
-}
-
-resource "google_compute_address" "app_ip" {
-  name = "reddit-app-ip"
+module "vpc" {
+  source = "modules/vpc"
 }
